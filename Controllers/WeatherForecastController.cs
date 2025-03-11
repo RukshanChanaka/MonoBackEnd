64c202310da5baeb7bd22e263c697f515d704c15
rukshandikovita <ruksh@RUKSHANPC>
2025-03-10 21:39:59 -0400

    tt

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorchingg"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorchinggg"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
